bb66cccad58c0689958ec940d09fd9375edc0203
Ryft <10768614+propavingk@users.noreply.github.com>
2014-05-26 17:15:00 -0500

    refactor: security

--- src/descriptorpin/canon.py
+++ src/descriptorpin/canon.py
@@ -5,3 +5,7 @@ or two versions of one server, may serialise the same descriptor with
 different key order or whitespace. Hashing the raw bytes would then report
 a change where none exists, or miss a real change hidden behind reordering.
 
+canon.py produces one deterministic byte string per descriptor so the hash
+in pin.py is stable against key order and insignificant whitespace, and
+sensitive only to the values that matter.
+"""
