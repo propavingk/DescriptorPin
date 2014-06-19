cfa0f9ede31a18bd27cff7579bb76b43aad61fc5
Ryft <10768614+propavingk@users.noreply.github.com>
2014-06-19 09:55:00 -0500

    feat: tool-poisoning

---
+++ src/descriptorpin/inventory.py
@@ -0,0 +1,11 @@
+"""Parse a server and tool inventory.
+
+An inventory is a JSON document describing the MCP servers a client is
+configured to talk to, and the tools each server exposes. descriptorpin
+reads an inventory both when pinning (recording the trusted state) and when
+scanning (checking the current state against the pin).
+
+The parser is strict about structure and forgiving about optional fields.
+It raises InventoryError with a specific message rather than letting a
+KeyError or TypeError escape, so the CLI can print a usage-grade message
+and exit 2.
