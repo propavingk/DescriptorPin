9f7298337d06aa44727990747d00eb077423f884
Ryft <10768614+propavingk@users.noreply.github.com>
2015-02-23 14:00:00 -0500

    refactor: supply-chain

--- src/descriptorpin/transport.py
+++ src/descriptorpin/transport.py
@@ -5,3 +5,8 @@ research on Model Context Protocol security found that a stdio server is
 launched as a local subprocess from client configuration, so a crafted
 configuration entry can turn a descriptor exchange into command execution
 on the host. The specification treats launching that subprocess as expected
+behaviour, so this is a design property rather than a bug with a coming fix.
+
+This module reports the transport of every server and marks stdio as a risk
+to be reviewed. It does not block or rate transports it cannot evaluate; an
+unknown transport is reported as unknown so the reader can look closer.
