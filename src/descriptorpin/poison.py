25f20b6bcac209c615efe9ae2c038d1d09e067f3
Ryft <10768614+propavingk@users.noreply.github.com>
2015-04-29 17:34:00 -0500

    chore: bump tool-poisoning

---
+++ src/descriptorpin/poison.py
@@ -0,0 +1,20 @@
+"""Structural signals of instruction text in a tool description.
+
+The poisoning shape is a tool description that carries instructions aimed
+at the model reading it, rather than documentation aimed at the human
+integrating the tool. A description is supposed to say what a tool does. A
+poisoned description tries to tell the model what to do.
+
+poison.py never returns a single opaque verdict. It runs a set of named,
+independently scored structural signals and reports which ones fired and
+where. A signal is a heuristic: it can fire on innocent text and it can
+miss a careful attacker. The reader judges. The module does not claim to
+detect malice, and it deliberately contains no working attack payloads.
+
+The signals are structural properties of instruction-shaped English, not a
+blocklist of known attacks:
+
+  imperative_opener      a sentence that opens with a bare imperative verb
+  second_person_model    direct address to the model or assistant
+  tool_reference         references to other tools by name-like tokens
+  concealment            instructions to hide, ignore, or not mention
