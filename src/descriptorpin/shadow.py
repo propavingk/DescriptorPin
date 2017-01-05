8db12aa0afce6575be0f6c7123b62deb1c2681f7
Ryft <10768614+propavingk@users.noreply.github.com>
2017-01-05 15:23:00 -0500

    chore: bump mcp-security

--- src/descriptorpin/shadow.py
+++ src/descriptorpin/shadow.py
@@ -3,3 +3,10 @@
 The shadowing shape is two or more servers exposing a tool with the same
 name. A client that resolves a tool by bare name must pick one, and the
 losing definition is shadowed. An attacker who registers a second server
+with a colliding name can intercept calls the operator believed were going
+elsewhere, or can define the tool the client actually binds to.
+
+shadow.py groups tools by bare name across all servers and reports every
+name claimed by two or more servers. It states which servers collide and
+notes precedence risk: the client's resolution order decides the winner,
+and that order is often undocumented, so the collision alone is the finding.
