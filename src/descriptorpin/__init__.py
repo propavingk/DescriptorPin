c02c19b948e0c3f835cd39886e9b980dc3d39cf6
Ryft <10768614+propavingk@users.noreply.github.com>
2017-01-25 17:21:00 -0500

    build: integrity

--- src/descriptorpin/__init__.py
+++ src/descriptorpin/__init__.py
@@ -1,3 +1,4 @@
 """descriptorpin: a defensive integrity monitor for MCP tool descriptors.
 
 descriptorpin pins a canonical hash of every tool descriptor (name,
+description, input schema) from a Model Context Protocol server inventory,
