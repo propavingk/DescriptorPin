ba1b69c9add144683ba6b67b6c81aa24b12151cf
Ryft <10768614+propavingk@users.noreply.github.com>
2017-02-13 15:15:00 -0500

    fix: correct pinning

--- src/descriptorpin/__init__.py
+++ src/descriptorpin/__init__.py
@@ -2,3 +2,5 @@
 
 descriptorpin pins a canonical hash of every tool descriptor (name,
 description, input schema) from a Model Context Protocol server inventory,
+then on a later scan reports silent mutation (rug pull shape), cross-server
+name collisions (shadowing shape), instruction-shaped text in descriptions
