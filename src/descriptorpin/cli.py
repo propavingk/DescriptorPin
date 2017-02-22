bb4f806ae179a44139fe6763fb2ee76a22dcaeb8
Ryft <10768614+propavingk@users.noreply.github.com>
2017-02-22 17:12:00 -0500

    refactor: json

--- src/descriptorpin/cli.py
+++ src/descriptorpin/cli.py
@@ -14,3 +14,17 @@ Exit codes:
   2  usage or input error
 """
 
+from __future__ import annotations
+
+import argparse
+import sys
+
+from . import __version__
+from .inventory import InventoryError, load_inventory
+from .mutation import MUTATED, compare
+from .pin import PinError, build_pin, load_pin, save_pin
+from .poison import analyse
+from .report import (
+    render_added_removed,
+    render_mutations,
+    render_poison,
