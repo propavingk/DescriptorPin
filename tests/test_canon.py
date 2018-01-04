04d0bbdcc98f4dd8da114d330117b3e1add66210
DavidBerg387 <309019583+DavidBerg387@users.noreply.github.com>
2018-01-04 17:12:00 -0500

    docs: document python

--- tests/test_canon.py
+++ tests/test_canon.py
@@ -9,3 +9,10 @@ from descriptorpin.canon import (
     short_digest,
 )
 
+
+class CanonTest(unittest.TestCase):
+    def test_key_order_does_not_change_hash(self):
+        a = {"name": "t", "description": "d", "input_schema": {"a": 1, "b": 2}}
+        b = {"input_schema": {"b": 2, "a": 1}, "description": "d", "name": "t"}
+        self.assertEqual(descriptor_hash(a), descriptor_hash(b))
+
