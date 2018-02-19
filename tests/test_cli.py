1fb537a9754f94e811c3e754615d88b6c156f4d6
Ryft <10768614+propavingk@users.noreply.github.com>
2018-02-19 15:48:00 -0500

    fix: cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,3 +5,11 @@ import os
 import tempfile
 import unittest
 from contextlib import redirect_stdout
+
+from descriptorpin.cli import main
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+SAMPLES = os.path.join(os.path.dirname(HERE), "samples")
+CLEAN = os.path.join(SAMPLES, "inventory_clean.json")
+MUTATED = os.path.join(SAMPLES, "inventory_mutated.json")
+PIN = os.path.join(SAMPLES, "pin.json")
