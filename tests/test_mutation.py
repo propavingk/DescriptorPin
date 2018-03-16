e1bfb0a98c670d976b2e9e6b066e3f9741fcdfaa
Ryft <10768614+propavingk@users.noreply.github.com>
2018-03-16 15:15:00 -0500

    refactor: tidy tool-poisoning

--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -4,3 +4,10 @@ import unittest
 
 from descriptorpin.inventory import parse_inventory
 from descriptorpin.mutation import ADDED, MATCHED, MUTATED, REMOVED, compare, mutations
+from descriptorpin.pin import build_pin
+
+
+def _inv(description="original", extra=None):
+    tools = [{"name": "t", "description": description, "input_schema": {"type": "object"}}]
+    if extra:
+        tools.append(extra)
