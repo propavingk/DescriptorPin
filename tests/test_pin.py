67374fb327c30b5bd204a329d6498b10daa7d33e
Ryft <10768614+propavingk@users.noreply.github.com>
2019-01-16 13:15:00 -0500

    feat: add integrity

--- tests/test_pin.py
+++ tests/test_pin.py
@@ -11,3 +11,11 @@ def _inv():
     return parse_inventory(
         {
             "servers": [
+                {
+                    "name": "s1",
+                    "transport": "http",
+                    "tools": [
+                        {"name": "b", "description": "second", "input_schema": {}},
+                        {"name": "a", "description": "first", "input_schema": {}},
+                    ],
+                }
