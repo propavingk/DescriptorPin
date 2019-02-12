721af2bccce3baa5599c7d7038b6a80346f83123
Ryft <10768614+propavingk@users.noreply.github.com>
2019-02-12 18:42:00 -0500

    refactor: tidy mcp

--- tests/test_poison.py
+++ tests/test_poison.py
@@ -5,3 +5,10 @@ The point is to exercise the structural detectors: imperative mood aimed at
 the model, concealment language, precedence assertions, and so on.
 """
 
+import unittest
+
+from descriptorpin.poison import analyse
+
+
+class PoisonTest(unittest.TestCase):
+    def test_plain_description_fires_nothing(self):
