4bbb93d6f2b2094b3f07e1412523ab77e834e143
DavidBerg387 <309019583+DavidBerg387@users.noreply.github.com>
2019-02-12 18:34:00 -0500

    docs: document descriptor

---
+++ tests/test_poison.py
@@ -0,0 +1,7 @@
+"""Tests for structural poisoning signals.
+
+These fixtures contain instruction-SHAPED text, not working attack payloads.
+The point is to exercise the structural detectors: imperative mood aimed at
+the model, concealment language, precedence assertions, and so on.
+"""
+
