27e017fccb0db6ce2dca151c2505871f7db1531b
Ryft <10768614+propavingk@users.noreply.github.com>
2019-02-14 17:33:00 -0500

    style: clean up descriptor

--- tests/test_shadow.py
+++ tests/test_shadow.py
@@ -9,3 +9,9 @@ from descriptorpin.shadow import collisions
 def _inv(servers):
     return parse_inventory({"servers": servers})
 
+
+def _server(name, tool_names):
+    return {
+        "name": name,
+        "transport": "http",
+        "tools": [{"name": t, "description": "d", "input_schema": {}} for t in tool_names],
